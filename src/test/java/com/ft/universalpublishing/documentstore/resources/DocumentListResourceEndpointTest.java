43dbf2a229a0fa094e33e801bf1b8693f00f8b52
Sandor Torok <sandor.torok@ft.com>
2015-08-07 12:10:40 +0300

    Fixed failing test

--- src/test/java/com/ft/universalpublishing/documentstore/resources/DocumentListResourceEndpointTest.java
+++ src/test/java/com/ft/universalpublishing/documentstore/resources/DocumentListResourceEndpointTest.java
@@ -136,11 +136,11 @@ public class DocumentListResourceEndpointTest {
     //DELETE
 
     @Test
-    public void shouldReturn204WhenDeletedSuccessfully() {
+    public void shouldReturn200WhenDeletedSuccessfully() {
         ClientResponse clientResponse = resources.client().resource(writePath)
                 .delete(ClientResponse.class);
 
-        assertThat("response", clientResponse, hasProperty("status", equalTo(204)));
+        assertThat("response", clientResponse, hasProperty("status", equalTo(200)));
     }
 
     @Test
