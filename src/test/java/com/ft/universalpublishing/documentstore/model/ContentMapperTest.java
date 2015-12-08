00120c756e55dd151b4a5c433b405673ca0529c5
Simon Gibbs <simon.gibbs@ft.com>
2015-12-08 13:39:18 +0000

    Added mappings for Copyright
     Conflicts:
    	src/test/java/com/ft/universalpublishing/documentstore/model/ContentMapperTest.java

--- src/test/java/com/ft/universalpublishing/documentstore/model/ContentMapperTest.java
+++ src/test/java/com/ft/universalpublishing/documentstore/model/ContentMapperTest.java
@@ -4,8 +4,11 @@ import com.ft.universalpublishing.documentstore.model.read.Uri;
 import com.ft.universalpublishing.documentstore.model.transformer.Brand;
 import com.ft.universalpublishing.documentstore.model.transformer.Comments;
 import com.ft.universalpublishing.documentstore.model.transformer.Content;
+import com.ft.universalpublishing.documentstore.model.transformer.Copyright;
 import com.ft.universalpublishing.documentstore.model.transformer.Identifier;
 import com.ft.universalpublishing.documentstore.model.transformer.Member;
+
+import org.fest.util.Strings;
 import org.joda.time.DateTime;
 import org.junit.Test;
 
@@ -14,6 +17,7 @@ import java.util.SortedSet;
 import java.util.TreeSet;
 import java.util.UUID;
 
+import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -61,6 +65,58 @@ public class ContentMapperTest {
         assertThat(readContent.getComments(), equalTo(new com.ft.universalpublishing.documentstore.model.read.Comments(true)));
         assertThat(readContent.getPublishReference(), equalTo("Publish Reference"));
     }
+    
+   
+    @Test
+    public void testLiveBlogContentMapping() throws Exception {
+        final UUID uuid = UUID.randomUUID();
+        final Date publishDate = new Date();
+        final String title = "Philosopher";
+        final String byline = "David Jules";
+
+        final SortedSet<Identifier> identifiers = new TreeSet<>();
+        identifiers.add(new Identifier("authority1", "identifier1"));
+        
+        final SortedSet<Brand> brands = new TreeSet<>();
+        brands.add(new Brand("Lex"));
+        brands.add(new Brand("Chuck Taylor"));
+        
+        final UUID mainImageUuid = UUID.randomUUID();
+        
+        final String ref = "Publish Reference";
+        
+        final Content content = Content.builder()
+                .withUuid(uuid)
+                .withTitle(title)
+                .withPublishedDate(publishDate)
+                .withByline(byline)
+                .withBrands(brands)
+                .withMainImage(mainImageUuid.toString())
+                .withIdentifiers(identifiers)
+                .withComments(new Comments(true))
+                .withRealtime(true)
+                .withPublishReference(ref)
+                .build();
+
+        final com.ft.universalpublishing.documentstore.model.read.Content readContent = mapper.map(content);
+
+        assertThat(readContent.getId(), equalTo("http://www.ft.com/thing/" + uuid.toString()));
+        assertThat(readContent.getTitle(), equalTo("Philosopher"));
+        assertThat(readContent.getPublishedDate(), equalTo(new DateTime(publishDate.getTime())));
+        assertThat(readContent.getType(), equalTo(TypeResolver.TYPE_ARTICLE));
+
+        assertThat(readContent.getByline(), equalTo("David Jules"));
+        assertThat(readContent.getIdentifiers().first(), equalTo(new com.ft.universalpublishing.documentstore.model.read.Identifier("authority1", "identifier1")));
+        final SortedSet<String> expectedBrands = new TreeSet<>();
+        expectedBrands.add("Lex");
+        expectedBrands.add("Chuck Taylor");
+        assertThat(readContent.getBrands(), equalTo(expectedBrands));
+        assertThat(readContent.getMainImage(), equalTo(new Uri("http://localhost/content/" + mainImageUuid.toString())));
+        assertThat(readContent.getComments(), equalTo(new com.ft.universalpublishing.documentstore.model.read.Comments(true)));
+        assertThat(readContent.getPublishReference(), equalTo("Publish Reference"));
+
+        assertThat(readContent.isRealtime(),equalTo(true));
+    }
 
     @Test
     public void testImageMapping() throws Exception {
@@ -77,6 +133,9 @@ public class ContentMapperTest {
                 .withInternalBinaryUrl("http://methode-image-binary-transformer/binary/" + uuid.toString())
                 .withExternalBinaryUrl("http://ft.s3.aws/" + uuid.toString())
                 .withIdentifiers(identifiers)
+                .withPixelWidth(1536)
+                .withPixelHeight(1538)
+                .withCopyright(new Copyright("© AFP"))
                 .build();
 
         final com.ft.universalpublishing.documentstore.model.read.Content readContent = mapper.map(content);
@@ -88,6 +147,9 @@ public class ContentMapperTest {
         assertThat(readContent.getByline(), equalTo("David Jules"));
         assertThat(readContent.getIdentifiers().first(), equalTo(new com.ft.universalpublishing.documentstore.model.read.Identifier("authority1", "identifier1")));
         assertThat(readContent.getBinaryUrl() , equalTo("http://ft.s3.aws/" + uuid.toString()));
+        assertThat(readContent.getPixelWidth() , equalTo(1536));
+        assertThat(readContent.getPixelHeight() , equalTo(1538));
+        assertThat(readContent.getCopyright().getNotice(),equalTo("© AFP"));
     }
 
     @Test
