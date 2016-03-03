73958f2632551e24bb7e0b6dfb2e2d4eb8adc556
Zoltan Bodor <Zoltan.Bodor@ft.com>
2016-03-03 15:06:24 +0200

    use absolute path

--- data-migration-scripts/operations-in-coco.sh
+++ data-migration-scripts/operations-in-coco.sh
@@ -7,7 +7,7 @@ sudo mv dump restore-data.js operations-in-container.sh /vol/mongodb
 
 echo "Running operations in MongoDB docker container"
 MONGO_CONTAINER_ID="$(docker ps | grep "coco/mongodb" | cut -d' ' -f1)"
-docker exec $MONGO_CONTAINER_ID data/db/operations-in-container.sh $PREFER_ARCHIVE
+docker exec $MONGO_CONTAINER_ID /data/db/operations-in-container.sh $PREFER_ARCHIVE
 
 echo "Cleaning Coco"
 sudo rm -r /vol/mongodb/dump /vol/mongodb/restore-data.js /vol/mongodb/operations-in-container.sh /home/core/operations-in-coco.sh
