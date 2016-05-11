f3db0283db4040ba114780bea8ba34e74b9c4fd8
Hunor Kovacs <hunor.kovacs@ft.com>
2016-05-11 12:32:21 +0100

    Brightcove contents will be returned as MediaResource

--- src/main/java/com/ft/universalpublishing/documentstore/model/TypeResolver.java
+++ src/main/java/com/ft/universalpublishing/documentstore/model/TypeResolver.java
@@ -4,16 +4,19 @@ import com.ft.universalpublishing.documentstore.model.transformer.Content;
 
 public class TypeResolver {
 
-    public static final String TYPE_ARTICLE = "http://www.ft.com/ontology/content/Article";
-    public static final String TYPE_IMAGE_SET = "http://www.ft.com/ontology/content/ImageSet";
-    public static final String TYPE_MEDIA_RESOURCE = "http://www.ft.com/ontology/content/MediaResource";
+    static final String BRIGHTCOVE_AUTHORITY = "http://api.ft.com/system/BRIGHTCOVE";
 
-    public String resolveType(Content source) {
+    static final String TYPE_ARTICLE = "http://www.ft.com/ontology/content/Article";
+    static final String TYPE_IMAGE_SET = "http://www.ft.com/ontology/content/ImageSet";
+    static final String TYPE_MEDIA_RESOURCE = "http://www.ft.com/ontology/content/MediaResource";
+
+    String resolveType(Content source) {
         if ((source.getBody() != null)
                 || ((source.isRealtime() != null) && source.isRealtime().booleanValue())) {
             return TYPE_ARTICLE;
         }
-        if (source.getInternalBinaryUrl() != null) {
+        if (source.getInternalBinaryUrl() != null ||
+                source.getIdentifiers() != null && !source.getIdentifiers().isEmpty() && source.getIdentifiers().first().getAuthority().equals(BRIGHTCOVE_AUTHORITY)) {
             return TYPE_MEDIA_RESOURCE;
         }
         if (source.getMembers() != null) {
