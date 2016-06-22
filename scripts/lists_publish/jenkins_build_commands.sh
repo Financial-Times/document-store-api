0700c33df16b3c182f9e30b28fc972439da03ea4
Jussi.Heinonen <jussi.heinonen@ft.com>
2016-06-22 16:24:12 +0100

    jenkins_build_commands.sh update. Look up credentials from file

--- scripts/lists_publish/jenkins_build_commands.sh
+++ scripts/lists_publish/jenkins_build_commands.sh
@@ -1,11 +1,44 @@
 #!/bin/bash
 
 JSONDOC="publish.json"
-BASE_API_URL['Dynpub']="https://dynpub-uk-up.ft.com/lists"
-BASE_API_URL['Pre_prod']="https://pre-prod-up.ft.com/lists"
-BASE_API_URL['Prod_uk']="https://prod-uk-up.ft.com/lists"
-BASE_API_URL['Prod_us']="https://prod-us-up.ft.com/lists"
+CREDENTIALS='/home/jenkins/coco.txt'
+CONNECT_TIMEOUT="3"
 
+
+declare -A BASE_API_URL
+BASE_API_URL['Dynpub']="https://dynpub-uk-up.ft.com/__document-store-api/lists/"
+BASE_API_URL['Pre_prod']="https://pre-prod-up.ft.com/__document-store-api/lists/"
+BASE_API_URL['Prod_uk']="https://prod-uk-up.ft.com/__document-store-api/lists/"
+BASE_API_URL['Prod_us']="https://prod-us-up.ft.com/__document-store-api/lists/"
+
+function getKeyValueFromFile () {
+  # Looks up value for key in format of key = value or key : value. 
+  # Separator can be specified as an argument
+  # Expects file to contain key only once
+  #
+  # arg1 = file name to look up
+  # arg2 = key to look up
+  # arg3 = key-value separator. Default is ' = '
+  #
+  # USAGE
+  # keyval=$(getKeyValueFromFile "${HOME}/.aws/credentials" "aws_access_key_id" "=")
+  # echo "Value retuned $keyval"
+
+  if [[ -z "$3" ]]; then
+    delimiter=" = "
+  else
+    delimiter="$3"
+  fi
+  key=$2
+
+  if [[ -f "$1" ]]; then
+    value=$(sed -n "s/^${key}${delimiter}//p" $1)
+    rtncode="$?"
+    if [[ "$rtncode" -eq "0" ]]; then      
+      echo $value
+    fi
+  fi
+}
 
 echo "Environment: ${ENVIRONMENT}"
 echo "Base API URL: ${BASE_API_URL[${ENVIRONMENT}]}"
@@ -15,18 +48,22 @@ echo "Layout Hint: ${LAYOUT_HINT}"
 echo "Title: ${TITLE}"
 echo "Transaction ID: ${TRANSACTION_ID}"
 
-if [ "${ENVIRONMENT}" =~ "Prod_" && "${PROD_RELEASE}" == "true" ]; then
+if [[ "${ENVIRONMENT}" =~ "Prod_" && "${PROD_RELEASE}" == "true" ]]; then
   echo "Production environment and PROD_RELEASE selected"
-elif [ "${ENVIRONMENT}" =~ "Prod_" && "${PROD_RELEASE}" == "false" ]; then
-  echo "PROD_RELEASE un-selected"
+elif [[ "${ENVIRONMENT}" =~ "Prod_" && "${PROD_RELEASE}" == "false" ]]; then
+  echo "PROD_RELEASE must be ticked in order to deploy to ${ENVIRONMENT}"
+  exit 1
 fi
 
 scripts/lists_publish/jsongen.py -t "${TITLE}" -i "${LIST_ID}" -c "${CONTENT_IDS}" -l "${LAYOUT_HINT}" -x "${TRANSACTION_ID}" > ${JSONDOC}
 
-if [[ "$?" -eq "0" ]]; then
-  #curl -m 1 --upload-file ${JSONDOC} ${ENDPOINT}
-  echo "Upload disabled"
+coco_credentials=$(getKeyValueFromFile "${CREDENTIALS}" "${ENVIRONMENT}")
+if [[ -n ${coco_credentials} ]]; then
+      #curl --user "${coco_credentials}" -m ${CONNECT_TIMEOUT} --upload-file ${JSONDOC} ${BASE_API_URL[${ENVIRONMENT}]}/${LIST_ID}
+      echo "Upload disabled"
+      echo "Curl parameters --user "${coco_credentials}" -m ${CONNECT_TIMEOUT} --upload-file ${JSONDOC} ${BASE_API_URL[${ENVIRONMENT}]}/${LIST_ID}"
 else
-  echo "Failed to generate ${JSONDOC}"
-  exit 1
+    echo "Failed to lookup credentials. Exit 1."
+    exit 1
 fi
+
