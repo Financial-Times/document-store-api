258c9a0e79805622cc1e66433f99f1efc065f440
Erdem Aslan <erdem.aslan@ft.com>
2018-05-04 15:28:43 +0100

    added actual healthcheck response checking in testing

--- src/test/java/com/ft/universalpublishing/documentstore/DocumentStoreApiApplicationTest.java
+++ src/test/java/com/ft/universalpublishing/documentstore/DocumentStoreApiApplicationTest.java
@@ -1,5 +1,7 @@
 package com.ft.universalpublishing.documentstore;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.dropwizard.testing.ResourceHelpers;
 import io.dropwizard.testing.junit.DropwizardAppRule;
 import org.glassfish.jersey.client.JerseyClientBuilder;
@@ -8,6 +10,7 @@ import org.junit.Test;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.core.Response;
+import java.io.IOException;
 
 import static org.junit.Assert.assertEquals;
 
@@ -19,7 +22,7 @@ public class DocumentStoreApiApplicationTest {
                     ResourceHelpers.resourceFilePath("config-no-mongo-test.yml"));
 
     @Test
-    public void applicationShouldRegisterHealthChecksEvenIfMongoConnectionFails() {
+    public void applicationShouldRegisterHealthChecksEvenIfMongoConnectionFails() throws IOException {
         Client client = new JerseyClientBuilder().build();
 
         Response response = client.target(
@@ -27,6 +30,14 @@ public class DocumentStoreApiApplicationTest {
                 .request()
                 .get();
 
+
+        final String payload = response.readEntity(String.class);
+        final JsonNode jsonNode = new ObjectMapper().readValue(payload, JsonNode.class);
+
+        // being sure that the node/field is valid/present in the response, default asBoolean call always returns false.
+        final String ok = jsonNode.at("/checks/0/ok").asText();
+
         assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
+        assertEquals("false", ok);
     }
 }
